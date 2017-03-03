6995957770efc047aad78cfd7e25f23b689be322
Samuel Valdez <savaldez@csumb.edu>
2017-03-03 00:53:09 +0000

    added html

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    
+    <div>
+        <div id = "inner">
+            
+            <form action="/index.php">
+                
+            How many passwords? <input type="text" name="passwords" size="3" maxlength = "5"><br>
+            
+            <input type="radio" name="sixChars" value="y" id="six"> <label for="cpcYes"> 6 characters </label>
+            <input type="radio" name="eightChars" value="n" id="eight"> <label for="cpcNo"> 8 characters  </label>
+            <input type="radio" name="tenChars" value="n" id="ten"> <label for="cpcNo">  10 characters </label>
+            
+            
+            <br/>
+            <select name = "letter">
+                <option value ="a">a</option>
+                <option value = "b">b</option>
+                <option value = "c">c</option>
+                <option value ="d">d</option>
+                <option value = "e">e</option>
+                <option value = "f">f</option>
+                <option value ="g">g</option>
+                <option value = "h">h</option>
+                <option value = "i">i</option>
+                <option value ="j">j</option>
+                <option value = "k">k</option>
+                <option value = "l">l</option>
+                <option value ="m">m</option>
+                <option value = "n">n</option>
+                <option value = "o">o</option>
+                <option value ="p">p</option>
+                <option value = "q">q</option>
+                <option value = "r">r</option>
+                <option value ="s">s</option>
+                <option value = "t">t</option>
+                <option value = "u">u</option>
+                <option value ="v">v</option>
+                <option value = "w">w</option>
+                <option value = "x">x</option>
+                <option value ="y">y</option>
+                <option value = "z">z</option>
+              
+            </select>
+            
+            <input type="submit" value="Submit">
+            </form>
+            
+        </div>
+    </div>
+    
+    <body>
+
+    </body>
+</html>
