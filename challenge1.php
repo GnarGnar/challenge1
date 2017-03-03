8d45c995b588322f3138f169894ab20052e7f03a
Kieran Burke <kburke@csumb.edu>
2017-03-03 00:52:33 +0000

    fixed

--- challenge1.php
+++ challenge1.php
@@ -5,13 +5,13 @@ function passwords($amount,$size,$exclude)
     $alpha= array('a','b','c','d','e','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
     shuffle($alpha);
     $passwords = array();
-    for(int $i =0; $i<amount;$i++)
+    for($i =0;$i<amount;$i++)
     {
-        for(int $j=0;$j<$size;$j++)
+        for($j=0;$j<$size;$j++)
         {
             if($alpha[$j] == $exclude)
             {
-                j++;
+                $j++;
                 array_push($alpha[$j]);
             }
             else
